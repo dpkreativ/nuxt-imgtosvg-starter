350850c1ba7fdfc515dd3d158da548fdf0f9388c
Divine Orji <dpkreativ@gmail.com>
2022-07-25 07:15:14 +0100

    feat: 🎸 Create components
    I created the Button, SectionBox, and Loading components

---
+++ components/SectionBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <article
+      class="h-64 w-full bg-gray-200 rounded-lg p-5 relative overflow-hidden"
+    >
+      <slot name="article" />
+    </article>
+
+    <div class="w-full flex flex-row-reverse justify-between my-5">
+      <slot name="buttons" />
+    </div>
+  </section>
+</template>
